2c72c4bd9884dc9371941e44849f8fd7a32b4308
AYUSH GOEL <ayush.goel501@gmail.com>
2019-02-09 16:58:58 +0530

    Offline support added

---
+++ app/src/main/java/com/example/newsrecent/newsrecent/fullNews.java
@@ -0,0 +1,29 @@
+package com.example.newsrecent.newsrecent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class fullNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_news);
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.setWebViewClient(new WebViewClient());
+        String uri = getIntent().getStringExtra("CLickedNewsText");
+        try {
+            URI myUri = new URI(uri);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        browser.loadUrl(uri);
+    }
+}
